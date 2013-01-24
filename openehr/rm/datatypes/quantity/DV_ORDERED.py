a3bef8ba78b3fa6a0120b4fabac8329c9dd5f240
chrispess <chrispess@gmail.com>
2013-01-24 19:53:57 -0200

    superclase generica data value

--- openehr/rm/datatypes/quantity/DV_ORDERED.py
+++ openehr/rm/datatypes/quantity/DV_ORDERED.py
@@ -1,9 +1,13 @@
 __author__ = 'chrispess'
 
 
+#classe criada para definir o conceito de valores ordenados
+
 from openehr.rm.datatypes.basic import DATA_VALUE
 
-class DV_ORDERED():
+class DV_ORDERED(DATA_VALUE):
+
+    pass
 
 
 
