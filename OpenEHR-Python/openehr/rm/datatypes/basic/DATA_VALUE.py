801d49df8c1837a7a57e6d9be234ab374688ce61
chrispess <chrispess@gmail.com>
2013-01-24 15:20:54 -0200

    classe quantity

--- OpenEHR-Python/openehr/rm/datatypes/basic/DATA_VALUE.py
+++ OpenEHR-Python/openehr/rm/datatypes/basic/DATA_VALUE.py
@@ -1,1 +1,16 @@
 __author__ = 'chrispess'
+
+
+#superclasse abstrata de todos os data_values: dv_boolean, dv_state, dv_identifier
+
+class DATA_VALUE:
+
+
+    def __init__(self):
+        self.__init__()
+
+
+
+    def __DATA_VALUE__(self):
+        self.__DATA_VALUE__()
+
