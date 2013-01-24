bb9ab6b509cf4a0748aebd85957d37a742765d79
chrispess <chrispess@gmail.com>
2013-01-24 20:04:31 -0200

    superclase generica data value

--- openehr/rm/datatypes/quantity/DV_QUANTIFIED.py
+++ openehr/rm/datatypes/quantity/DV_QUANTIFIED.py
@@ -1,5 +1,7 @@
 __author__ = 'chrispess'
 
+#superclasse abstrata que define o conceito de valores quantificáveis, ordenados e com precisão na magnitude
+
 from openehr.rm.datatypes.quantity import DV_ORDERED
 
 class DV_QUANTIFIED(DV_ORDERED):
