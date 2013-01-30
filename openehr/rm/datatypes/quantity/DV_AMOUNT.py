704cb82b3763fc0393cebc9bd2e43e8065b0ef2a
chrispess <chrispess@gmail.com>
2013-01-30 21:38:30 -0200

    classe do pacote quantity

--- openehr/rm/datatypes/quantity/DV_AMOUNT.py
+++ openehr/rm/datatypes/quantity/DV_AMOUNT.py
@@ -1,1 +1,17 @@
 __author__ = 'chrispess'
+
+
+from openehr.rm.datatypes.quantity.DV_QUANTIFIED import DV_QUANTIFIED
+
+class DV_AMOUNT(DV_QUANTIFIED):
+
+
+#atributos mandatórios
+
+accuracy = 0,0
+
+accuracyPercent = False
+
+
+
+
