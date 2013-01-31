80ff25df59fe3f3d51145edfbffeadc4e535a4b6
chrispess <chrispess@gmail.com>
2013-01-31 00:44:00 -0200

    classe do pacote basic

--- openehr/rm/datatypes/basic/DV_BOOLEAN.py
+++ openehr/rm/datatypes/basic/DV_BOOLEAN.py
@@ -1,1 +1,28 @@
 __author__ = 'chrispess'
+
+from openehr.rm.datatypes.basic import DATA_VALUE
+
+class DV_BOOLEAN(DATA_VALUE):
+
+    #atributos
+
+
+    #valor booleano do item
+    value = False
+
+
+    #construtor
+    def __init__(self, value):
+        self.value = value
+
+
+    #retorna o valor booleano do item
+    def getValue(self):
+        return self.value
+
+    #seta um valor booleano
+    def __set__(self, value):
+        self.value =value
+
+
+
