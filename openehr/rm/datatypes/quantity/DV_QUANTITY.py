9d98960206620d5dd4f4d02c70a440999aeba395
chrispess <chrispess@gmail.com>
2013-01-30 22:16:26 -0200

    classe do pacote quantity

--- openehr/rm/datatypes/quantity/DV_QUANTITY.py
+++ openehr/rm/datatypes/quantity/DV_QUANTITY.py
@@ -1,1 +1,16 @@
 __author__ = 'chrispess'
+
+
+from openehr.rm.datatypes.quantity import DV_AMOUNT
+
+
+class DV_QUANTITY(DV_AMOUNT):
+
+    #atributos
+
+    magnitude = 0.0  #double
+
+    precision = 0    # int
+
+    units = ""       # String
+
