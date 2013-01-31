6ec9d152461b77f0efd83482eadf92bd27a27c64
chrispess <chrispess@gmail.com>
2013-01-30 22:08:42 -0200

    classe do pacote quantity

--- openehr/rm/datatypes/quantity/DV_AMOUNT.py
+++ openehr/rm/datatypes/quantity/DV_AMOUNT.py
@@ -13,9 +13,23 @@ class DV_AMOUNT(DV_QUANTIFIED):
     accuracyPercent = False
 
 
+#construtor
+
 def __init__(self, accuracy, accuracyPercent):
     self.accuracy = accuracy
     self.accuracyPercent = accuracyPercent
+
+#retorna accuracy
+
+def getAccuracy(self):
+    return self.accuracy
+
+
+#retorna accuracyPercent
+
+def isAccuracyPercent(self):
+    return self.accuracyPercent
+
 
 
 
