7d0ebd0e88c56eee89144f00aa1d5b056b0e04dc
chrispess <chrispess@gmail.com>
2013-02-08 11:22:18 -0200

    classe do pacote identification

--- openehr/rm/support/identification/ARCHETYPE_ID.py
+++ openehr/rm/support/identification/ARCHETYPE_ID.py
@@ -5,34 +5,38 @@ from openehr.rm.support.identification import OBJECT_ID
 
 class ARCHETYPE_ID(OBJECT_ID):
 
-#referência, globalmente, uma entidade do modelo.
-# ex
- qualifiedRmEntity = str()
+    #referência, globalmente, uma entidade do modelo.
+    # ex openehr-composition-OBSERVATION
+    qualifiedRmEntity = str()
+
+    #organizaão de onde se origina o modelo de referência base do arquétipo.
+    #ex: "openehr", "cen", "hl7"
+    rmOriginator = str()
+
+    #nome do modelo de referência.
+    # ex: "en13606", "ehr_rm"
+    rmName = str()
+
+    #nome do nível ontológico do modelo de referência do qual o arquétipo se origina.
+    # ex: "section", "entry", "folder", "composition"
+    rmEntity = str()
+
+    #nomedoconceito representado pelo arquétipo, incluindo especializações.
+    # ex: "biochemistry_result-cholesterol"
+    domainConcept = str()
+
+    #nome da especialização do conceito, se o arquétipo for a especialização
+    # de outro arquétipo. ex: "cholesterol"
+    specialization = str()
+
+    #versão do arquétipo
+    versionID = str()
 
 
-#organizaão de onde se origina o modelo de referência base do arquétipo.
-#ex: "openehr", "cen", "hl7"
-rmOriginator = str()
-
-#nome do modelo de referência.
-# ex: "en13606", "ehr_rm"
-rmName = str()
+    def __init__(self, rm_originator, rm_name, rm_entity, domain_concept, specialization, version_id):
 
 
 
-#nome do nível ontológico do modelo de referência do qual o arquétipo se origina.
-# ex: "section", "entry", "folder", "composition"
-rmEntity = str()
-
-#nomedoconceito representado pelo arquétipo, incluindo especializações.
-# ex: "biochemistry_result-cholesterol"
-domainConcept = str()
-
-#nome da especialização do conceito, se o arquétipo for a especialização
-# de outro arquétipo. ex: "cholesterol"
-specialization = str()
 
 
-#vesão do arquétipo
-versionID = str()
 
