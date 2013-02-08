cc889a6b8b72e796328d54b62ba5acf3d502757d
chrispess <chrispess@gmail.com>
2013-02-08 10:51:32 -0200

    classe do pacote identification

--- openehr/rm/support/identification/ARCHETYPE_ID.py
+++ openehr/rm/support/identification/ARCHETYPE_ID.py
@@ -1,1 +1,38 @@
 __author__ = 'chrispess'
+
+
+from openehr.rm.support.identification import OBJECT_ID
+
+class ARCHETYPE_ID(OBJECT_ID):
+
+#referência, globalmente, uma entidade do modelo.
+# ex
+qualifiedRmEntity = str()
+
+
+#organizaão de onde se origina o modelo de referência base do arquétipo.
+#ex: "openehr", "cen", "hl7"
+rmOriginator = str()
+
+#nome do modelo de referência.
+# ex: "en13606", "ehr_rm"
+rmName = str()
+
+
+
+#nome do nível ontológico do modelo de referência do qual o arquétipo se origina.
+# ex: "section", "entry", "folder", "composition"
+rmEntity = str()
+
+#nomedoconceito representado pelo arquétipo, incluindo especializações.
+# ex: "biochemistry_result-cholesterol"
+domainConcept = str()
+
+#nome da especialização do conceito, se o arquétipo for a especialização
+# de outro arquétipo. ex: "cholesterol"
+specialization = str()
+
+
+#vesão do arquétipo
+versionID = str()
+
