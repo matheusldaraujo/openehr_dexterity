607184a810fbf0f68ff665abb1d98fcdfa47bdd7
chrispess <chrispess@gmail.com>
2013-02-09 20:04:03 -0200

    classe do pacote identification

--- openehr/rm/datatypes/text/DV_CODED_TEXT.py
+++ openehr/rm/datatypes/text/DV_CODED_TEXT.py
@@ -2,6 +2,48 @@
 
 __author__ = 'chrispess'
 
+from openehr.rm.datatypes.text.DV_TEXT import DV_TEXT
+from openehr.rm.datatypes.text.CODE_PHRASE import CODE_PHRASE
+
+#um item de texto cujo valor pode ser uma rubrica de uma terminologia controlada,
+# em suma um DV_CODED_TEXT é a combinaão de uma CODE_PHRASE (um código) e a rubrica do termo,
+# de um serviço de terminologias na linguagem em que foi autorado.
+
+class DV_CODED_TEXT(DV_TEXT):
+
+
+ defining_code = CODE_PHRASE() #a rubrica associada ao  'value'
+
+
+ #construtor
+ def __init__(self,definingCode, value, mappings, formatting, hyperlink, language, encoding):
+
+     self.defining_code=definingCode  #este self valora o atributo defining_code definido acima
+
+
+     super(DV_CODED_TEXT, self).__init__()
+     self.value = value  #este self chama o atributo 'value' da superclasse DV_TEXT
+     #demais atributos da super classe DV_TEXT
+     self.mappings = mappings
+     self.formatting = formatting
+     self.hyperlink = hyperlink
+     self.language = language
+     self.encoding = encoding
+     self.language = language
+
+
+
+
+     #seta value e defining_code
+ def setDefining_code(self,value, defining_code):
+     self.defining_code - defining_code
+     super(DV_CODED_TEXT,self).__init__()
+     self.value= value
+
+ #seta defining_code
+  def setDefining_code(self, defining_code):
+      self.defining_code - defining_code
+
 
 
 
