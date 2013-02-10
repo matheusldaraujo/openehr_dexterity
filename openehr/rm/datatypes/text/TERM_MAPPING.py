f32db1fd9427ea3a912f92d83f1d02af21480aec
chrispess <chrispess@gmail.com>
2013-02-10 00:17:55 -0200

    classe do pacote text

--- openehr/rm/datatypes/text/TERM_MAPPING.py
+++ openehr/rm/datatypes/text/TERM_MAPPING.py
@@ -1,4 +1,30 @@
 __author__ = 'chrispess'
 
+from openehr.rm.datatypes.text.CODE_PHRASE import CODE_PHRASE
+from openehr.rm.datatypes.text.DV_CODED_TEXT import DV_CODED_TEXT
+from openehr.BASIC_TYPES import typechar
+
+
+
 class TERM_MAPPING():
-    pass
+
+  # o termo alvo do mapeamento
+  target = CODE_PHRASE()
+
+  #correspondência relativa do termo alvo com respeito ao item de texto mapeado.
+  # os resultados podem ter os seguintes significados:
+  # '>': o mapeamento é mais amplo. ex: texto original = "arbovirus infection"
+  # e o target = 'viral infection'
+  # '=': mapeamento éequivalente ao termo original
+  # '<': o mapeamento é menos amplo. ex: textooriginal = 'diabetes'
+  # e o target = 'diabetes mellitus'
+  #'?': desconhecido
+  match = typechar()
+
+  #proposito do mapeamento. ex: "interoperabilidade", "automação de data mining"...
+  purpose =  DV_CODED_TEXT()
+
+
+  def __init__(self,target,match,purpose):
+
+
