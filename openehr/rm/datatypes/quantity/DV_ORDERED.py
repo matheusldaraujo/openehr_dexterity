c407759cc32627c42ef57385398f7cefa7281acf
chrispess <chrispess@gmail.com>
2013-02-11 20:31:36 -0200

    Classe DV_ORDERED pacote quantity

--- openehr/rm/datatypes/quantity/DV_ORDERED.py
+++ openehr/rm/datatypes/quantity/DV_ORDERED.py
@@ -15,10 +15,14 @@ from openehr.BASIC_TYPES import typeStr
 #classe abstrata que define o conceito de valores ordenados
 class DV_ORDERED(DATA_VALUE):
 
+  #valor opcional para um range normal
   normal_range = DV_INTERVAL(0,0)
 
+  #lista de ranges opcionais para este valor neste contexto, em particular
   otherReferenceRanges = DV_INTERVAL[]
 
+  #opcional. indicador normal de status do valorcom respeito ao range normal
+  # ver OpenEHR terminology group "normal status"
   normal_status = CODE_PHRASE(teminology_id = TERMINOLOGY_ID(), code_string = typeStr)
 
 
