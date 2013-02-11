b314b8029f1c80a7f9a0222eb6e62d8c31f983f3
chrispess <chrispess@gmail.com>
2013-02-10 22:11:56 -0200

    classe do pacote text

--- openehr/rm/datatypes/text/DV_PARAGRAPH.py
+++ openehr/rm/datatypes/text/DV_PARAGRAPH.py
@@ -1,1 +1,10 @@
 __author__ = 'chrispess'
+
+from  openehr.rm.datatypes.basic.DATA_VALUE import DATA_VALUE
+from  openehr.rm.datatypes.text.DV_TEXT import DV_TEXT
+
+class DV_PARAGRAPH(DATA_VALUE):
+
+
+  items = DV_TEXT[]
+
