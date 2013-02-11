614a66933de860dbb50b8450a524b92511577de8
chrispess <chrispess@gmail.com>
2013-02-11 17:25:58 -0200

    classe do pacote text

--- openehr/rm/datatypes/basic/DV_STATE.py
+++ openehr/rm/datatypes/basic/DV_STATE.py
@@ -1,1 +1,13 @@
 __author__ = 'chrispess'
+
+
+from openehr.rm.datatypes.text.DV_CODED_TEXT import DV_CODED_TEXT
+from openehr.BASIC_TYPES import typeBool
+
+#nome do estado. determinados pela tabela'state/event'
+# codificada utilizando terminologia OpenEHR
+value = DV_CODED_TEXT() #value != vazio
+
+#indica quando é um estado terminal, como "aborted","completed", etc
+is_terminal = typeBool() # is_terminal != vazio
+
