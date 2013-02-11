157fc280cae7820bcd47dae95f81aea4afdf5bb9
chrispess <chrispess@gmail.com>
2013-02-11 19:44:10 -0200

    Classe DV_INTERVAL pacote quantity

--- openehr/rm/datatypes/quantity/DV_INTERVAL.py
+++ openehr/rm/datatypes/quantity/DV_INTERVAL.py
@@ -1,1 +1,7 @@
 __author__ = 'chrispess'
+
+from openehr.rm.support.basic.Interval import Interval
+
+#classe genérica para definião de um intervalo.
+# Usada para definir intervalos de datas, tempo, quantidades
+
