67b73d0a9293c7d449e37a47dab82111d564c62f
chrispess <chrispess@gmail.com>
2013-02-12 21:46:30 -0200

    Classe DV_PROPORTION  pacote quantity

--- openehr/rm/datatypes/quantity/DV_PROPORTION.py
+++ openehr/rm/datatypes/quantity/DV_PROPORTION.py
@@ -9,7 +9,17 @@ from openehr.rm.datatypes.quantity.PROPORTION_KIND import PROPORTION_KIND
 
 class DV_PROPORTION(DV_AMOUNT):
 
+   #numerador da razão
    numerator = typeFloat()
+
+   #denominador da razão
    denominator = typeFloat()
+
+   #indica o tipo da proporção
    type = PROPORTION_KIND()
+
+   #expressa a precisão em termos decasas decimais. 0 -> integral,
+   # -1 -> qualquer número decasas decimais
    precision = typeInt()
+
+
