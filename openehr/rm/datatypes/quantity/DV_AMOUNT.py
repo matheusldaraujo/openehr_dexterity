a39cae108b06cb365db8d76d9eb2b28c6136112c
chrispess <chrispess@gmail.com>
2013-02-12 20:14:11 -0200

    Classe DV_ORDINAL DV_QUANTIFIED  pacote quantity

--- openehr/rm/datatypes/quantity/DV_AMOUNT.py
+++ openehr/rm/datatypes/quantity/DV_AMOUNT.py
@@ -3,37 +3,43 @@ __author__ = 'chrispess'
 
 
 from openehr.rm.datatypes.quantity.DV_QUANTIFIED import DV_QUANTIFIED
+from openehr.BASIC_TYPES import typeFloat
+ from openehr.BASIC_TYPES import typeBool
+
 
 class DV_AMOUNT(DV_QUANTIFIED):
 
 
 #atributos mandatórios
 
-    accuracy = 0,0
+    #precisão da medida, expressa como percentagem. o valor zero (0) indica
+    # que a precisão não foi registrada
+    accuracy = typeFloat
 
-    accuracyPercent = False
+    #se verdadeira indica que a precisão foi registrada.
+    accuracy_is_percent = typeBool
 
 
 #construtor
-
-def __init__(self, accuracy, accuracyPercent):
+def __init__(self, accuracy, accuracy_is_percent):
     self.accuracy = accuracy
-    self.accuracyPercent = accuracyPercent
+    self.accuracy_is_percent = accuracy_is_percent
 
 #retorna accuracy
-
 def getAccuracy(self):
     return self.accuracy
 
 
 #retorna accuracyPercent
-
-def isAccuracyPercent(self):
-    return self.accuracyPercent
-
+def getAccuracyPercent(self):
+    return self.accuracy_is_percent
 
 
-
+#true se a percentagem é válida
+def valid_percentage(self, value =typeFloat()):
+    if(value >=0.0 and value <= 0.0):
+        return True
+    else:return False
 
 
 
