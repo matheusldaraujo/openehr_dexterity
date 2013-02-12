c3de675f2514b56a1f11a53b26b862a441f4c989
chrispess <chrispess@gmail.com>
2013-02-12 21:11:50 -0200

    Classe DV_PROPORTION  pacote quantity

--- openehr/rm/datatypes/quantity/DV_PROPORTION.py
+++ openehr/rm/datatypes/quantity/DV_PROPORTION.py
@@ -1,1 +1,15 @@
 __author__ = 'chrispess'
+
+
+from openehr.BASIC_TYPES import typeFloat
+from openehr.BASIC_TYPES import typeInt
+from openehr.rm.datatypes.quantity.DV_AMOUNT import DV_AMOUNT
+from openehr.rm.datatypes.quantity.PROPORTION_KIND import PROPORTION_KIND
+
+
+class DV_PROPORTION(DV_AMOUNT):
+
+   numerator = typeFloat()
+   denominator = typeFloat()
+   type = PROPORTION_KIND()
+   precision = typeInt()
