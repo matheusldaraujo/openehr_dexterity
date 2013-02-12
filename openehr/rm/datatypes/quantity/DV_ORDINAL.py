1facb0260c0b2556f6425c8bae95932be3f90460
chrispess <chrispess@gmail.com>
2013-02-12 18:39:40 -0200

    Classe DV_ORDINAL DV_QUANTIFIED  pacote quantity

--- openehr/rm/datatypes/quantity/DV_ORDINAL.py
+++ openehr/rm/datatypes/quantity/DV_ORDINAL.py
@@ -1,2 +1,7 @@
 __author__ = 'chrispess'
 
+from openehr.BASIC_TYPES import typeInt
+from openehr.rm.datatypes.text.DV_CODED_TEXT import DV_CODED_TEXT
+from openehr.rm.datatypes.quantity.DV_ORDERED import DV_ORDERED
+
+class DV_ORDINAL(DV_ORDERED):
