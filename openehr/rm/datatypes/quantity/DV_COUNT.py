cd88c61cee0947203270f47912c09f1d6f3ca69f
chrispess <chrispess@gmail.com>
2013-02-12 22:20:20 -0200

    Classe DV_COUNT  pacote quantity

--- openehr/rm/datatypes/quantity/DV_COUNT.py
+++ openehr/rm/datatypes/quantity/DV_COUNT.py
@@ -1,1 +1,20 @@
 __author__ = 'chrispess'
+
+
+from openehr.rm.datatypes.quantity.DV_AMOUNT import DV_AMOUNT
+from openehr.BASIC_TYPES import typeInt
+
+
+class DV_COUNT(DV_AMOUNT):
+
+    #representa a magnitude numérica da quantidade
+    magnitude = typeInt()
+
+    #construtor
+    def __init__(self, magnitude):
+        self.magnitude = magnitude
+
+
+
+
+
