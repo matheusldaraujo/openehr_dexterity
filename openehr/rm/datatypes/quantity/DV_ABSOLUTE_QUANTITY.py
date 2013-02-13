33382d6f7fcbe4b13d15dae9671116f57c972283
chrispess <chrispess@gmail.com>
2013-02-12 23:05:29 -0200

    Classe DV_ABSOLUTE_QUANTITY pacote quantity

--- openehr/rm/datatypes/quantity/DV_ABSOLUTE_QUANTITY.py
+++ openehr/rm/datatypes/quantity/DV_ABSOLUTE_QUANTITY.py
@@ -1,1 +1,9 @@
 __author__ = 'chrispess'
+
+from openehr.rm.datatypes.quantity.DV_QUANTIFIED import DV_QUANTIFIED
+
+
+#classe para definir o conceito de quantidades
+# cujos valores são absolutos com respeito à origem...
+# ex: datas e tempo
+class DV_ABSOLUTE_QUANTITY(DV_QUANTIFIED):
