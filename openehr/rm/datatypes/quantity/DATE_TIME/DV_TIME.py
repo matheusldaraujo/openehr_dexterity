6f8f0cb4482895e9a2a1448c1bc9c27e558236bf
chrispess <chrispess@gmail.com>
2013-02-13 20:58:54 -0200

    Classe DV_TEMPORAL pacote quantity/date_time

--- openehr/rm/datatypes/quantity/DATE_TIME/DV_TIME.py
+++ openehr/rm/datatypes/quantity/DATE_TIME/DV_TIME.py
@@ -1,1 +1,23 @@
 __author__ = 'chrispess'
+
+from  openehr.rm.datatypes.quantity.DATE_TIME.DV_TEMPORAL import Dv_TEMPORAL
+from  openehr.BASIC_TYPES import typeStr
+
+class DvTime(Dv_TEMPORAL):
+
+    #ISO 8601 time string
+    value = typeStr
+
+    #construtor
+    def __init__(self, value):
+        self.value = value
+
+
+    #seta value
+    def setValue(self, value):
+        self.value = value
+
+
+    #retorna valor numérico do tempo em segundos desde o início do dia
+    def magnitude(self):
+        pass
