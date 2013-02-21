1ae8067c61aa10d018f5f2585d0cb7bd59ff0340
chrispess <chrispess@gmail.com>
2013-02-21 16:08:23 -0300

    Classes do pacote quantity/date_time

--- openehr/rm/datatypes/quantity/DATE_TIME/DV_DATE_TIME.py
+++ openehr/rm/datatypes/quantity/DATE_TIME/DV_DATE_TIME.py
@@ -1,1 +1,15 @@
 __author__ = 'chrispess'
+
+from datetime import datetime
+from datetime import date
+from openehr.rm.datatypes.quantity.DATE_TIME.DV_TEMPORAL import Dv_TEMPORAL
+
+class DV_DATE_TIME(Dv_TEMPORAL):
+
+    #data e tempo na forma YYYY-mm-dd  HH:mm:ss
+    value = datetime
+
+    def __init__(self, ano, mes, dia, hora, minuto , segundo):
+        #
+        self.value = datetime(ano, mes, dia, hora, minuto,segundo)
+
