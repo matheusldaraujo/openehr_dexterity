f7cbd7d37a106d34ef4010d6c7f75f8d0cce7fa6
chrispess <chrispess@gmail.com>
2013-02-27 16:39:32 -0300

    Classes do pacote quantity/date_time

--- openehr/rm/datatypes/encapsulated/DV_PARSABLE.py
+++ openehr/rm/datatypes/encapsulated/DV_PARSABLE.py
@@ -1,1 +1,8 @@
 __author__ = 'chrispess'
+
+
+from openehr.BASIC_TYPES import typeInt
+from openehr.BASIC_TYPES import typeStr
+from openehr.rm.datatypes.encapsulated.DV_ENCAPSULATED import DV_ENCAPSULATED
+
+
