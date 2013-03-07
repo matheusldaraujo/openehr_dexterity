671c3127da00319cad6f6ec850d4c024cc26e60a
chrispess <chrispess@gmail.com>
2013-03-06 23:21:58 -0300

    Classes do pacote  DATA_STRUCTURE

--- openehr/rm/data_structure/ITEM_STRUCTURE/REPRESENTATION/ELEMENT.py
+++ openehr/rm/data_structure/ITEM_STRUCTURE/REPRESENTATION/ELEMENT.py
@@ -1,1 +1,29 @@
 __author__ = 'chrispess'
+
+from openehr.rm.datatypes.text.DV_CODED_TEXT import DV_CODED_TEXT
+from openehr.rm.datatypes.basic.DATA_VALUE import DATA_VALUE
+from openehr.rm.data_structure.ITEM_STRUCTURE.REPRESENTATION.ITEM import ITEM
+
+class ELEMENT (ITEM):
+
+ # data value desse nodo
+ value = DATA_VALUE
+
+ #tipo do null value: "indeterminado", "não perguntado"
+ null_flavour = DV_CODED_TEXT()
+
+ #construtor
+ def __init__(self,value, null_flavour ):
+     self.value = value
+     self.null_flavour= null_flavour
+
+
+ #seta value
+ def setValue(self, value = DATA_VALUE):
+     self.value = value
+
+ #seta null_flavour
+ def setNull_flavour(self, null_flavour = DV_CODED_TEXT):
+     self.null_flavour = null_flavour
+
+
