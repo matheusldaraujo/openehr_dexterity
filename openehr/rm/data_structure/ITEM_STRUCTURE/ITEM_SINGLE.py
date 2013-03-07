fffd65189b803d523f7938228f0931aa655bda38
chrispess <chrispess@gmail.com>
2013-03-06 23:56:54 -0300

    Classes do pacote  DATA_STRUCTURE/item_structure

--- openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_SINGLE.py
+++ openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_SINGLE.py
@@ -1,1 +1,11 @@
 __author__ = 'chrispess'
+
+
+from openehr.rm.data_structure.ITEM_STRUCTURE.REPRESENTATION.ELEMENT import ELEMENT
+from openehr.rm.data_structure.ITEM_STRUCTURE.ITEM_STRUCTURE import ITEM_STRUCTURE
+from openehr.rm.datatypes.basic.DATA_VALUE import DATA_VALUE
+from openehr.rm.datatypes.text.DV_CODED_TEXT import DV_CODED_TEXT
+class ITEM_SINGLE(ITEM_STRUCTURE):
+
+ item = ELEMENT(value = DATA_VALUE, null_flavour = DV_CODED_TEXT)
+
