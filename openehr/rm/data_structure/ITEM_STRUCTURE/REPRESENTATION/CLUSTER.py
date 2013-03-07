d6d9fde4a4fb27046490dcaffb6964baa6c8fc29
chrispess <chrispess@gmail.com>
2013-03-06 23:34:50 -0300

    Classes do pacote  DATA_STRUCTURE/REPRESENTATION

--- openehr/rm/data_structure/ITEM_STRUCTURE/REPRESENTATION/CLUSTER.py
+++ openehr/rm/data_structure/ITEM_STRUCTURE/REPRESENTATION/CLUSTER.py
@@ -1,1 +1,23 @@
 __author__ = 'chrispess'
+
+from openehr.rm.data_structure.ITEM_STRUCTURE.REPRESENTATION.ITEM import ITEM
+
+class CLUSTER(ITEM):
+
+    #lista ordenada de items:clusters ou elements
+    items = []
+
+    #construtor
+    def __init__(self, items):
+        self.items = items
+
+    #retorna a lista de  items
+    def getItems(self):
+        return self.items
+
+    #seta a lista de items
+    def setItems(self,items = []):
+        self.items = items
+
+
+
