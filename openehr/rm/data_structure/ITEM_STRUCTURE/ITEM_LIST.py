27670f5a96357eadef8a0214a5a2d3e871f02b5c
chrispess <chrispess@gmail.com>
2013-03-08 22:43:17 -0300

    Classes do pacote  DATA_STRUCTURE/item_structure

--- openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_LIST.py
+++ openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_LIST.py
@@ -1,10 +1,11 @@
 __author__ = 'chrispess'
 
 from openehr.rm.data_structure.ITEM_STRUCTURE.ITEM_STRUCTURE import ITEM_STRUCTURE
+from openehr.rm.data_structure.ITEM_STRUCTURE.REPRESENTATION.ELEMENT import ELEMENT
 from openehr.BASIC_TYPES import typeInt
 class ITEM_LIST(ITEM_STRUCTURE):
 
-    #representação de uma listade elementos
+    #representação de uma lista de elementos
     items = []
 
     #consturtor
@@ -25,6 +26,12 @@ class ITEM_LIST(ITEM_STRUCTURE):
     def getIthItem(self, i = typeInt):
         return self.items[i]
 
+    #retorna uma lista dos nomes dos elementos da ITEM_LIST
+    def names(self):
+        listNames = []
+        for ELEMENT in self.items:
+            listNames.append(ELEMENT.getName())
+        return listNames
 
 
     #gera um item  compatível com oISO 13606
