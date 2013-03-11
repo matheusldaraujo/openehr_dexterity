de5e6ae952bd607ec35c062b91c55ca804746bb5
chrispess <chrispess@gmail.com>
2013-03-11 15:08:26 -0300

    Classes do pacote  DATA_STRUCTURE/item_structure

--- openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_TABLE.py
+++ openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_TABLE.py
@@ -2,10 +2,13 @@ __author__ = 'chrispess'
 
 from openehr.rm.data_structure.ITEM_STRUCTURE.REPRESENTATION.CLUSTER import CLUSTER
 
-
+#data structure do tipo table
 
 class ITEM_TABLE(CLUSTER):
 
  rows = CLUSTER[]
 
- def __init__(self):
+ #construtor
+ def __init__(self, rows = CLUSTER[]):
+     self.rows = rows
+
