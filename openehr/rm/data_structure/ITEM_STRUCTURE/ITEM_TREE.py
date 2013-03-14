7afe08535a078c224b31de728f52555b238d031d
chrispess <chrispess@gmail.com>
2013-03-13 23:05:56 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_TABLE.py

--- openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_TREE.py
+++ openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_TREE.py
@@ -1,1 +1,14 @@
 __author__ = 'chrispess'
+
+from openehr.rm.data_structure.DATA_STRUCTURE import DATA_STRUCTURE
+from openehr.rm.data_structure.ITEM_STRUCTURE.REPRESENTATION.ITEM import ITEM
+
+class ITEM_TREE(ITEM):
+
+    items = []
+
+    #construtor
+    def __init__(self, items = []):
+        self.items = items
+
+
