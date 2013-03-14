ff58259f6a9f788fefd6f5abcdfad084e3b3df01
chrispess <chrispess@gmail.com>
2013-03-14 00:49:54 -0300

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	openehr/rm/data_structure/ITEM_STRUCTURE/ITEM_TABLE.py

--- openehr/rm/data_structure/HISTORY/HISTORY.py
+++ openehr/rm/data_structure/HISTORY/HISTORY.py
@@ -1,3 +1,10 @@
 __author__ = 'chrispess'
 
 from openehr.rm.data_structure.DATA_STRUCTURE import DATA_STRUCTURE
+from openehr.rm.datatypes.quantity.DATE_TIME.DV_DATE_TIME import DV_DATE_TIME
+from openehr.BASIC_TYPES import typeInt
+
+#objeto raiz de uma  história linear
+class HISTORY(DATA_STRUCTURE):
+
+ origin = DV_DATE_TIME(dia = typeInt, hora = typeInt, minuto = typeInt , segundo = typeInt)
