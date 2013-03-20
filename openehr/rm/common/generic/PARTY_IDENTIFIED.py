c1386fbd7275e8d176cec978850b0af9d4b150f2
chrispess <chrispess@gmail.com>
2013-03-20 17:47:53 -0300

    pacote common

--- openehr/rm/common/generic/PARTY_IDENTIFIED.py
+++ openehr/rm/common/generic/PARTY_IDENTIFIED.py
@@ -1,1 +1,20 @@
 __author__ = 'chrispess'
+
+from openehr.BASIC_TYPES import typeStr
+from openehr.rm.datatypes.basic.DV_IDENTIFIER import DV_IDENTIFIER
+
+class PARTY_IDENTIFIED(object):
+   #opcional, nome legível
+   name = typeStr
+
+   #um ou mais identificadores
+   identifiers = DV_IDENTIFIER[]
+
+   #construtor
+   def __init__(self, *args):
+       if(len(args)==1):
+           self.name = args[0]
+
+       elif(len(args)==2):
+           self.name = args[0]
+           self.identifiers = args[1]
