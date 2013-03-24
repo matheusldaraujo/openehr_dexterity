da2f6ff50af8305d71137b643b31bf2b30621d63
chrispess <chrispess@gmail.com>
2013-03-24 18:07:37 -0300

    pacote common

--- openehr/rm/common/generic/PARTY_PROXY.py
+++ openehr/rm/common/generic/PARTY_PROXY.py
@@ -8,3 +8,6 @@ class PARTY_PROXY():
     #0..1. referência opcional a maiores detalhes de identificção ou demográficos
     external_ref = PARTY_REF(id = OBJECT_REF(typeStr),namespace = typeStr, type = typeStr)
 
+    #construtor
+    def __init__(self, external_ref = PARTY_REF(id = OBJECT_REF(typeStr),namespace = typeStr, type = typeStr)):
+        self.external_ref = external_ref
