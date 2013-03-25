46df519eae0911fb792c179c2c482c6c8686ee6b
chrispess <chrispess@gmail.com>
2013-03-25 17:22:12 -0300

    pacote ENTRY

--- openehr/rm/composition/content/entry/ENTRY.py
+++ openehr/rm/composition/content/entry/ENTRY.py
@@ -8,7 +8,10 @@ from openehr.BASIC_TYPES import typeBool
 from openehr.BASIC_TYPES import typeStr
 from openehr.rm.common.generic.PARTY_PROXY import PARTY_PROXY
 from openehr.rm.composition.content.CONTENT_ITEM import CONTENT_ITEM
+from openehr.rm.common.generic.PARTICIPATION import PARTICIPATION
 from openehr.rm.support.identification.TERMINOLOGY_ID import TERMINOLOGY_ID
+from openehr.rm.support.identification.OBJECT_ID  import OBJECT_ID
+
 
 
 class ENTRY(CONTENT_ITEM):
@@ -23,4 +26,28 @@ class ENTRY(CONTENT_ITEM):
     #Id da pessoa que é sujeito da ENTRY.
     # ex: doador de órgãos, feto, membro da família, etc
     subject = PARTY_PROXY(external_ref = PARTY_REF(id = OBJECT_REF(typeStr),namespace = typeStr, type = typeStr))
+
+    #identificação opcional do provedor dasinformações da ENTRY.
+    # ex: o paciente, parente ou guardião, o clínico, dispositivo ou software
+    provider = PARTY_PROXY(external_ref = PARTY_REF(id = OBJECT_REF(typeStr),namespace = typeStr, type = typeStr))
+
+    #outros participantes
+    other_participations = PARTICIPATION[]
+
+    #identificador de workflow externo
+    workflow_id = OBJECT_REF(id = OBJECT_ID(id = typeStr), namespace = typeStr, type = typeStr)
+
+    def __init__(self, *args):
+        if(len(args)==3):
+            self.language = args[0]
+            self.encoding = args[1]
+            self.subject =args[2]
+        elif(len(args)>3):
+            self.language = args[0]
+            self.encoding = args[1]
+            self.subject =args[2]
+            self.provider = args[3]
+            self.other_participations = args[4]
+            self.workflow_id = args[5]
+
 
